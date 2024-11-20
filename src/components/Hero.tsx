4a805c935b63055bd8652b49fe5d060bd01b05fa
zaheem hassan <164178436+zaheem55@users.noreply.github.com>
2024-11-20 17:50:40 +0500

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Image from 'next/image'
-import profile from "../../publish/zaheem_porfile.jpeg"
 
 const Hero = () => {
   return (
@@ -10,7 +9,7 @@ const Hero = () => {
       <div className='container grid lg:grid-cols-2 h-[calc(100vh-60px)]' >
         
         <div className='hidden lg:block'>
-        <Image src={profile} alt='' height={600} width={500}/>
+        <Image src= "zaheem_porfile.jpeg" alt='' height={600} width={500}/>
           
         </div>
         <div className='text-[100px] sm:text-[130px] font-bold leading-tight flex justify-center items-center'>
